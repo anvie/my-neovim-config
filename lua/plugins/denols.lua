3ea4af2a73fd38692daf6073168877968331e9b0
anvie <robinsyihab@gmail.com>
2024-10-23 09:35:23 +0700

    add plugins: denols, obsidian

---
+++ lua/plugins/denols.lua
@@ -0,0 +1,9 @@
+return {
+  "sigmasd/deno-nvim",
+  config = function()
+    -- Example mapping to toggle outline
+    require("deno-nvim").setup({
+      -- Your setup opts here (leave empty to use defaults)
+    })
+  end,
+}
